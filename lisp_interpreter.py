0694a694fcd996d802e0a3e64698f55b50835a02
Kumar Kartikay <krkartikay@gmail.com>
2018-01-11 21:11:10 +0530

    some progress on the tree parser

--- lisp_interpreter.py
+++ lisp_interpreter.py
@@ -1,8 +1,10 @@
 class LispStatement():
     "A single statement in Lisp"
-    def __init__(self,statement, env=globalEnv):
+    def __init__(self,statement, env=None):
         "Creates internal representation of lisp statement"
         self.statement = statement
+        if env is None:
+            env = LispEnvironment() # Create a global env
         self.environment = env
         self.tree = createTree(self.statement)
     def evaluate(self):
@@ -17,8 +19,16 @@ class LispStatement():
 
 class LispEnvironment():
     "Environment for lisp execution"
-    def __init__(self):
-        self.arg = arg
+    def __init__(self, outer=None):
+        # create a global env if called directly,
+        # otherwise a sub-environment
+        self.outer = outer
+        if outer is None:
+            self.name = "GlobalEnv"
+        else:
+            # do something like inheriting variables from outside
+            pass
+
 def tokeniser(string):
     "A generator that yields tokens based on input"
     NUMBER = 1
@@ -54,6 +64,38 @@ def tokeniser(string):
         mode = newmode
     yield forming_token
 
-def createTree(string):
+def createTree(string, rootNode=None):
     "Creates a tree from a string like (+ (- 2 3) (a))"
-    pass
+    cur_node = TreeNode('') # root node
+    for token in tokeniser(string):
+        if token == '(':
+            new_root = TreeNode('')
+            cur_node.addNodeBelow(new_root)
+            cur_node = new_root
+        elif token == ')':
+            cur_node = cur_node.above
+        else:
+            cur_node.addNodeBelow(TreeNode(token))
+    return cur_node
+
+class TreeNode:
+    "A single Node in a Tree"
+    def __init__(self, token):
+        self.token = token
+        self.above = None
+        self.below = []
+    def addNodeBelow(self, node):
+        self.above = self
+        self.below.append(node)
+    def toList(self):
+        if len(self.below) == 0:
+            try:
+                return int(self.token)
+            except ValueError:
+                return self.token
+        l = self.below
+        for i in range(len(l)):
+            x = l[i]
+            if isinstance(x,TreeNode):
+                l[i] = x.toList()
+        return l
