f5cbe14b26e7842ed986267f55b724d67ad7b550
Kumar Kartikay <krkartikay@gmail.com>
2018-01-11 22:14:43 +0530

    starting to think abt main interpreter code

--- lisp_interpreter.py
+++ lisp_interpreter.py
@@ -1,24 +1,5 @@
 import tree_parse
 
-class LispStatement():
-    "A single statement in Lisp"
-    def __init__(self,statement, env=None):
-        "Creates internal representation of lisp statement"
-        self.statement = statement
-        if env is None:
-            env = LispEnvironment() # Create a global env
-        self.environment = env
-        self.tree = tree_parse.createTree(self.statement).toList()
-    def evaluate(self):
-        "Evaluates the statement and returns its value"
-        if type(self.tree) == list:
-            # do something recursively?
-            # creating a new environment or something?
-            pass
-        else:
-            # it is already a primitive value
-            return self.tree
-
 class LispEnvironment():
     "Environment for lisp execution"
     def __init__(self, outer=None):
@@ -26,7 +7,44 @@ class LispEnvironment():
         # otherwise a sub-environment
         self.outer = outer
         if outer is None:
-            self.name = "GlobalEnv"
+            # define global environment
+            self.globalEnv = True
         else:
             # do something like inheriting variables from outside
             pass
+
+globalEnv = LispEnvironment()
+
+class LispStatement():
+    "The internal representation of a lisp statement"
+    def __init__(self, statement, env=None):
+        if type(statement) == list:
+            self.tree = statement
+        elif type(statement) == str:
+            self.tree = tree_parse.createTree(statement).toList()
+        else:
+            raise TypeError("Provided statement should be str or list")
+        if env is None:
+            env = globalEnv
+    def evaluate(self):
+        """
+        Evaluates the statement and returns its value.
+        This is done according to the following steps:
+        - Primitive Data elements return their own value
+        - Primitive Procedures evaluate to themselves
+        - Special forms are handled specially
+        - To evaluate an application:
+          1. Evaluate the operator
+          2. Evaluate the operands
+          3. Apply the procedure, by:
+             a. Copying the body of the procedure;
+                while substituting formal parameters
+             b. Evaluating the new body
+        """
+        if type(self.tree) == list:
+            # do something recursively?
+            # creating a new environment or something?
+            pass
+        else:
+            # it is already a primitive value
+            return self.tree
