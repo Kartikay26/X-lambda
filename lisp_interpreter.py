af3540ddf85bbc94f56f1171ccaf950847bb3cb2
Kumar Kartikay <krkartikay@gmail.com>
2018-01-11 18:39:27 +0530

    Made tokeniser

--- lisp_interpreter.py
+++ lisp_interpreter.py
@@ -19,6 +19,40 @@ class LispEnvironment():
     "Environment for lisp execution"
     def __init__(self):
         self.arg = arg
+def tokeniser(string):
+    "A generator that yields tokens based on input"
+    NUMBER = 1
+    STRING = 2
+    WHITESPACE = 3
+    SYMBOL = 4
+    def getmode(char):
+        modes = {NUMBER: '1234567890',
+                 WHITESPACE: ' \t\n',
+                 SYMBOL: '()'}
+        for m in modes:
+            if char in modes[m]:
+                return m
+        # else ... default mode is STRING
+        return STRING
+
+    mode = WHITESPACE
+    forming_token = ""
+    for ch in string:
+        newmode = getmode(ch)
+        if newmode == mode:
+            if mode != SYMBOL:
+                forming_token += ch
+            else:
+                yield forming_token
+                forming_token = ch
+        else:
+            if forming_token.strip() != '':
+                yield forming_token
+            else:
+                pass
+            forming_token = ch
+        mode = newmode
+    yield forming_token
 
 def createTree(string):
     "Creates a tree from a string like (+ (- 2 3) (a))"
