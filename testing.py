9ed839f19f4213de11a535be8f4e75c086b068eb
Kumar Kartikay <krkartikay@gmail.com>
2018-01-13 09:56:17 +0530

    More tests

--- testing.py
+++ testing.py
@@ -3,6 +3,23 @@ import lisp_interpreter
 import tree_parse
 import unittest
 
+class TestEvaluator(unittest.TestCase):
+    "Test simple functioning of LispStatement class"
+    def testIntegerStatement(self):
+        statement = '2'
+        result = lisp_interpreter.LispStatement(
+                        tree_parse.createTree(statement).toList()
+                    ).evaluate()
+        expected = 2
+        self.assertEqual(expected, result)
+    def testArithmeticStatement(self):
+        statement = '(+ 2 2)'
+        result = lisp_interpreter.LispStatement(
+                        tree_parse.createTree(statement).toList()
+                    ).evaluate()
+        expected = 4
+        self.assertEqual(expected, result)
+
 class TestProcedures(unittest.TestCase):
     "Test functioning of LispProcedure class"
     def testSimpleProcedure(self):
